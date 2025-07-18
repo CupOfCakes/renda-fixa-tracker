7c353bda52d2fbdf1ced6cd28059cd2ae38524f9
Heitor <heitor.scarv@gmail.com>
2025-07-18 13:23:35 -0300

    cdi extractor

--- extractor/ext_rate.py
+++ extractor/ext_rate.py
@@ -1,9 +1,8 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
 
-
 def get_selic_rate():
-    current_date = datetime.now().date().strftime("%d/%m/%Y")
+    current_date = datetime.now().strftime("%d/%m/%Y")
 
     url_selic = ("https://api.bcb.gov.br/dados/serie/bcdata.sgs.432/dados?formato=json&"
                  f"dataInicial={current_date}&dataFinal={current_date}")
@@ -14,9 +13,33 @@ def get_selic_rate():
         selic = selic.json()
 
         if not selic:
-            return "NU" 
+            return "NU"
 
         return selic[0]
 
     except Exception as e:
         return "Erro ao obter a taxa Selic: ", e
+
+
+def get_cdi_rate():
+    for i in range(2):  
+        date = (datetime.now() - timedelta(days=i)).strftime("%d/%m/%Y")
+
+        url_cdi = (
+            f"https://api.bcb.gov.br/dados/serie/bcdata.sgs.4389/dados?"
+            f"formato=json&dataInicial={date}&dataFinal={date}"
+        )
+
+        try:
+            response = requests.get(url_cdi)
+            response.raise_for_status()
+            data = response.json()
+
+            if data:
+                return data[0]
+
+        except requests.RequestException as e:
+            #print(f"Erro ao obter CDI em {date}: {e}")
+            pass
+
+    return "NU"
